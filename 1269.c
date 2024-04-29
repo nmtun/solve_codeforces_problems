dbb3010da2228cf5f248b83c50612b0dcf2b0ea6
Nguyen Manh Tung <163404922+nmtun@users.noreply.github.com>
2024-04-29 09:46:37 +0700

    Add files via upload

---
+++ 1269.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+long long int n;
+int main () {
+    scanf("%lld",&n);
+    printf("%lld %lld",9*n,8*n);
+}
---
+++ 1285A.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+long long int n;
+char s[1000000];
+int main () {
+    scanf("%lld\n",&n);
+    scanf("%s",&s);
+    printf("%lld",n+1);
+
+}
